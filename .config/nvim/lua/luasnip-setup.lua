b89ecce0389fff97d0233b90fdb641c1b02e78ad
Vladimir Danilin <volodymyr.danilin@gmail.com>
2025-04-08 15:37:45 +0200

    feat(nvim): refactor luasnip

--- .config/nvim/lua/luasnip-setup.lua
+++ .config/nvim/lua/luasnip-setup.lua
@@ -1,36 +1,59 @@
 -- https://sbulav.github.io/vim/neovim-setting-up-luasnip/
-local ls = require('luasnip')
-local snip = ls.snippet
-local insert = ls.insert_node
-local text = ls.text_node
+-- https://ejmastnak.com/tutorials/vim-latex/luasnip/
+local luasnip = require('luasnip')
+local snippet = luasnip.snippet
+local insert_node = luasnip.insert_node
+local text_node = luasnip.text_node
 
-local cl_snippet = snip({
+local cl_snippet = snippet({
   trig = 'cl',
   namr = 'Console log',
   dscr = 'Console log',
 }, {
-  text('console.log('),
-  insert(1, 'data'),
-  text(');'),
+  text_node('console.log('),
+  insert_node(1, 'data'),
+  text_node(');'),
 })
 
-local clj_snippet = snip({
+local clj_snippet = snippet({
   trig = 'clj',
   namr = 'Console log JSON',
   dscr = 'Console log formatted JSON',
 }, {
-  text('console.log(JSON.stringify('),
-  insert(1, 'data'),
-  text(', null, 2));'),
+  text_node('console.log(JSON.stringify('),
+  insert_node(1, 'data'),
+  text_node(', null, 2));'),
 })
 
-ls.add_snippets(nil, {
+local stt_snippet = snippet({
+  trig = 'stt',
+  namr = 'Set timeout and try catch',
+  dscr = 'Set timeout and try catch',
+}, {
+  text_node({
+    'setTimeout(async () => {',
+    '  try {',
+    '',
+  }),
+  insert_node(1, 'data'),
+  text_node({
+    '',
+    '  } catch (error) {',
+    '    console.log(error)',
+    '  }',
+    '}, 2000)',
+  }),
+})
+
+luasnip.add_snippets(nil, {
   typescript = {
     cl_snippet,
     clj_snippet,
+    stt_snippet,
   },
   javascript = {
     cl_snippet,
     clj_snippet,
+    stt_snippet,
   },
 })
