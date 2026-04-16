dd2b52a2e55468a9dadef382836eb325d62eaecf
Vladimir Danilin <volodymyr.danilin@gmail.com>
2026-04-16 17:25:56 +0200

    feat(nvim): migrate to 0.12

--- .config/nvim/lua/nvim-treesitter-setup.lua
+++ .config/nvim/lua/nvim-treesitter-setup.lua
@@ -1,134 +1,56 @@
-require('nvim-treesitter.configs').setup({
-  -- A list of parser names, or "all"
-  ensure_installed = {
-    'angular',
-    'bash',
-    'c',
-    'css',
-    'csv',
-    'diff',
-    'dockerfile',
-    'fish',
-    'go',
-    'gomod',
-    'gowork',
-    'html',
-    'http',
-    'javascript',
-    'json',
-    'lua',
-    'make',
-    'markdown',
-    'query',
-    'rust',
-    'scss',
-    'terraform',
-    'tmux',
-    'toml',
-    'tsx',
-    'typescript',
-    'vim',
-    'vimdoc', -- help
-    'yaml',
-  },
+local treesitter = require('nvim-treesitter')
+local treesitter_config = require('nvim-treesitter.config')
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = true,
-
-  -- List of parsers to ignore installing (for "all")
-  -- ignore_install = { "javascript" },
-  highlight = {
-    -- `false` will disable the whole extension
-    enable = true,
-
-    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-    -- the name of the parser)
-    -- list of language that will be disabled
-    -- disable = { "c", "rust" },
-
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
-  indent = true,
+vim.api.nvim_create_autocmd('FileType', {
+  callback = function()
+    -- Enable treesitter highlighting and disable regex syntax
+    pcall(vim.treesitter.start)
+    -- Enable treesitter-based indentation
+    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+  end,
 })
 
-require('treesitter-context').setup({
-  enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-  max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
-  trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-  patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
-    -- For all filetypes
-    -- Note that setting an entry here replaces all other patterns for this entry.
-    -- By setting the 'default' entry below, you can control which nodes you want to
-    -- appear in the context window.
-    default = {
-      'class',
-      'function',
-      'method',
-      -- 'for',
-      -- 'while',
-      -- 'if',
-      -- 'switch',
-      -- 'case',
-    },
-    -- Patterns for specific filetypes
-    -- If a pattern is missing, *open a PR* so everyone can benefit.
-    tex = {
-      'chapter',
-      'section',
-      'subsection',
-      'subsubsection',
-    },
-    rust = {
-      'impl_item',
-      'struct',
-      'enum',
-    },
-    scala = {
-      'object_definition',
-    },
-    vhdl = {
-      'process_statement',
-      'architecture_body',
-      'entity_declaration',
-    },
-    markdown = {
-      'section',
-    },
-    elixir = {
-      'anonymous_function',
-      'arguments',
-      'block',
-      'do_block',
-      'list',
-      'map',
-      'tuple',
-      'quoted_content',
-    },
-    json = {
-      'pair',
-    },
-    yaml = {
-      'block_mapping_pair',
-    },
-  },
-  exact_patterns = {
-    -- Example for a specific filetype with Lua patterns
-    -- Treat patterns.rust as a Lua pattern (i.e "^impl_item$" will
-    -- exactly match "impl_item" only)
-    -- rust = true,
-  },
+local ensure_installed = {
+  'angular',
+  'bash',
+  'c',
+  'css',
+  'csv',
+  'diff',
+  'dockerfile',
+  'fish',
+  'go',
+  'gomod',
+  'gowork',
+  'html',
+  'http',
+  'javascript',
+  'json',
+  'lua',
+  'make',
+  'markdown',
+  'query',
+  'rust',
+  'scss',
+  'terraform',
+  'tmux',
+  'toml',
+  'tsx',
+  'typescript',
+  'vim',
+  'vimdoc', -- help
+  'yaml',
+}
 
-  -- [!] The options below are exposed but shouldn't require your attention,
-  --     you can safely ignore them.
+local already_installed = treesitter_config.get_installed()
 
-  zindex = 20, -- The Z-index of the context window
-  mode = 'cursor', -- Line used to calculate context. Choices: 'cursor', 'topline'
-  -- Separator between context and content. Should be a single character string, like '-'.
-  -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
-  separator = nil,
-})
+local parsers_to_install = vim
+  .iter(ensure_installed)
+  :filter(function(parser)
+    return not vim.tbl_contains(already_installed, parser)
+  end)
+  :totable()
+
+treesitter.install(parsers_to_install)
+
+treesitter.setup()
