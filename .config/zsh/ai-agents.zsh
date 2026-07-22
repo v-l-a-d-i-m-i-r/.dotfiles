1cb9c69ddbe480d784b7a5be47b1a8955c05c191
Vladimir Danilin <volodymyr.danilin@gmail.com>
2026-07-22 16:25:18 +0200

    feat(ai-agents): move from aliases to bin, make executor configurable in do-tasks-by-prd

--- .config/zsh/ai-agents.zsh
+++ .config/zsh/ai-agents.zsh
@@ -11,30 +11,6 @@ gemini() {
     "$NVM_DIR/versions/node/${GEMINI_CLI_NODE_VERSION}/bin/gemini" "$@"
 }
 
-# https://github.com/github/copilot-cli
-copilot() {
-  if [ -z "$COPILOT_CLI_NODE_VERSION" ]; then
-    echo 'Error: COPILOT_CLI_NODE_VERSION is not set.';
-    return 1;
-  fi
-
-  cp ${HOME}/.agents/AGENTS.md ${HOME}/.copilot/copilot-instructions.md;
-
-  PATH="$NVM_DIR/versions/node/${COPILOT_CLI_NODE_VERSION}/bin:$PATH" \
-    "$NVM_DIR/versions/node/${COPILOT_CLI_NODE_VERSION}/bin/copilot" "$@"
-}
-
-copilot-ralph() {
-  # https://docs.github.com/en/copilot/how-tos/copilot-cli/set-up-copilot-cli/configure-copilot-cli
-  copilot \
-    --allow-all-tools \
-    --allow-all-paths \
-    --deny-tool='shell(rm)' \
-    --deny-tool='shell(git push)' \
-    --silent \
-    $@;
-}
-
 # https://github.com/openai/codex
 codex() {
   if [ -z "$CODEX_CLI_NODE_VERSION" ]; then
@@ -108,20 +84,4 @@ opencode() {
 
   PATH="$NVM_DIR/versions/node/${OPENCODE_CLI_NODE_VERSION}/bin:$PATH" \
     "$NVM_DIR/versions/node/${OPENCODE_CLI_NODE_VERSION}/bin/opencode" "$@"
-}
-
-vibe() {
-  cp ${HOME}/.agents/AGENTS.md ${HOME}/.vibe/AGENTS.md
-
-  ${HOME}/.local/bin/vibe $@;
-}
-
-vibe-ralph() {
-  cp ${HOME}/.agents/AGENTS.md ${HOME}/.vibe/AGENTS.md
-
-  # ignores cofig from $VIBE_HOME/config.toml
-  # VIBE_HOME="${HOME}/.vibe-ralph" \
-  ${HOME}/.local/bin/vibe \
-    --trust \
-    $@;
 }
--- justfile
+++ justfile
@@ -9,4 +9,5 @@ init:
 # Format
 format:
   @./.bin/pretify-json ./.claude/settings.json
+  @./.bin/pretify-json ./.copilot/settings.json
   @make -C .config/nvim format
